877eb6ebf34e1ca40b8f21abe7305432d33e0a75
AbhinasDash <abhinasdash143@gmail.com>
2023-04-27 11:51:44 +0530

    show hide cart

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,15 @@
-import classes from './CartButton.module.css';
+import { useDispatch } from "react-redux";
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
 
 const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const toggleCart = () => {
+    dispatch(uiActions.toggle());
+  };
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCart}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
